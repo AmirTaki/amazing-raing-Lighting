e35f3945b61de64581217c08aae500e2f533c5ea
amir <amirtaki623@gmail.com>
2025-08-03 23:22:26 +0330

    main js part 6

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ const rain = () => {
     const cloud = document.querySelector(".cloud")
     const e = document.createElement("div")
     const left = Math.floor(Math.random() * 310)
+    let width =  Math.random() * 5
 
     e.classList.add("drop");
     cloud.append(e)
     e.style.left = left + "px"
+    e.style.width = .5 + width + "px"
 
     setTimeout(()=> {
         cloud.removeChild(e)
